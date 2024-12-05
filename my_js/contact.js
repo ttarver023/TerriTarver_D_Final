07fe88281370afb68ca46377f8b42e8524a8dec3
Terri Tarver <132149739+ttarver023@users.noreply.github.com>
2024-12-05 13:59:17 -0500

    Add files via upload

--- my_js/contact.js
+++ my_js/contact.js
@@ -1,1 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
+    const responseMessage = document.getElementById("responseMessage");
+    const submitButton = document.getElementById("submitButton");
 
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        submitButton.value = 'Submitting...';
+        submitButton.disabled = true; 
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        // const formData = new FormData(contactForm);
+        const formData = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message,
+        };
+
+        const apiEndpoint = window.location.hostname === "localhost" ? "http://localhost:3000/api/contact" : "https://backend-app-8ev9.onrender.com/api/contact";
+
+        fetch(apiEndpoint, {
+            method: "POST",
+            headers: {
+                'Content-Type':
+                    'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                submitButton.value = 'Submit';
+                submitButton.disabled = false; // Re-enable the button
+                responseMessage.innerHTML = `<p>${data.message}</p>`;
+                responseMessage.classList.add("success", "show"); 
+
+                setTimeout(() => {
+                    contactForm.reset();
+                    responseMessage.innerHTML = "";
+                    responseMessage.classList.remove("success", "show");
+                }, 2000);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                submitButton.value = 'Submit';
+                submitButton.disabled = false; // Re-enable the button
+                responseMessage.innerHTML = `<p>Error submitting the form. Please try again later.</p>`;
+                responseMessage.classList.add("error", "show"); 
+
+                setTimeout(() => {
+                    responseMessage.innerHTML = "";
+                    responseMessage.classList.remove("error", "show");
+                }, 3000);
+            });
+    });
+});
---
+++ my_js/subscribe.js
@@ -0,0 +1,78 @@
+$('.next').click(function(){
+  var nextTarget = $(this).parent().siblings('span');
+  var currentTarget = $(this).parent();
+  currentTarget.removeClass('active');
+  nextTarget.addClass('active').find('input').focus();
+});
+
+$('input#email').on('keydown',function(e){
+  var keyCode = e.keyCode || e.which; 
+  if (keyCode == 13) {
+    $('.submit').trigger('click');
+  }
+})
+
+$('.submit').click(function(){
+  var target = $(this);
+  var lastInputContainerLabel = target.parent().find('.container.active label');
+  target.addClass('submitted');
+  lastInputContainerLabel.addClass('fadeOut');
+})
+
+
+
+// capture tab press
+$(".container").on('keydown', 'input', function(e) { 
+  var keyCode = e.keyCode || e.which; 
+
+  if (keyCode == 9) { 
+    e.preventDefault();
+    //tabbing backwards
+    if (e.shiftKey) {
+      var currentInput = $(this);
+      var prevInput = currentInput.parent().prev('.container').find('input');
+      var currentContainer = currentInput.parent();
+      var prevContainer = currentInput.parent().prev('.container');
+      if (currentContainer.is(':first-of-type')){
+        // do nothing if is first input
+      } else {
+        currentContainer.removeClass('active');
+        prevContainer.addClass('active');
+        prevInput.focus();
+      }
+      //tabbing forwards
+    } else {
+      var currentInput = $(this);
+      var nextInput = currentInput.parent().next('.container').find('input');
+      var currentContainer = currentInput.parent();
+      var nextContainer = currentInput.parent().next('.container');
+      if (currentContainer.is(':nth-last-of-type(2)')){
+        // do nothing if is last input
+      } else {
+        currentContainer.removeClass('active');
+        nextContainer.addClass('active');
+        nextInput.focus();
+      }
+    }
+  } 
+});
+
+
+// validate the form
+$('.newsletter').validate({
+  errorElement: "span"
+});
+
+
+// reset the form
+$('.reset').click(function(){
+  var target = $('form.newsletter');
+  var targetInput = target.find('span.container > input');
+  var inputContainer = target.find('span');
+  var firstContainer = target.find('span.container:first-of-type');
+  targetInput.val('');
+  inputContainer.removeClass('active');
+  firstContainer.addClass('active');
+  $('span.submit').removeClass('submitted');
+  $('label').removeClass('fadeOut')
+});
